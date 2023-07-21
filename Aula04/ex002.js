03f3479fcece07044a204d683c29efaa61248425
Kauandlr <kauanlara123@gmail.com>
2023-07-21 16:15:10 -0300

    Verificar

---
+++ Aula04/ex002.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil'){ //Se país for diferente de Brasil ele é Estrangeiro
+    console.log(`Você mora no ${país}`)
+}
+else{
+    console.log("Você é Estrangeiro")
+}
