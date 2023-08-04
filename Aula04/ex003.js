65f6fc3384e813fd1a955681c8959fe9c96fb211
Kauandlr <kauanlara123@gmail.com>
2023-08-04 16:42:39 -0300

    js

---
+++ Aula04/ex003.js
@@ -0,0 +1,9 @@
+var idade = 22
+console.log (`Você tem ${idade} anos`)
+if (idade < 16){
+    console.log('Você está abaixo da idade')
+}else if (idade < 18  || idade >= 65){
+    console.log('Voto Opcional')
+}else {
+    console.log ('voto obrigatório')
+}
