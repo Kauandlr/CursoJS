58d753a3248a9accd393ee19fe5cd2a336bd206a
Kauandlr <kauanlara123@gmail.com>
2023-08-18 17:19:09 -0300

    Ajustes

--- Aula05/script.js
+++ Aula05/script.js
@@ -1,7 +1,18 @@
 function carregar(){
-var msg = window.document.getElementById('msg')
-var image = window.document.getElementById('foto')
+    var msg = window.document.getElementById('msg')
+    var imagem = window.document.getElementById('imagem')
+    var data = new Date()
 
-var data = new Date()
-var hora = data.getHours
+    //var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas`
+
+
+    if(hora >= 0 && hora < 12){
+        img.src = 'img/manha.png'
+    } else if(hora >= 12 && hora < 18){
+        img.src = 'img/tarde.png'
+    }else{
+        img.src = 'img/noite.png'
+    }
 }
